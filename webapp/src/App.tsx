b849ca6bf3e08c9e6c02a04e139efa40f79ca42f
evxeen <evxeen@gmail.com>
2025-02-18 10:53:44 +0300

    test lint-stage

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,5 +1,10 @@
 import { TrpcProvider } from '../lib/trpc'
-import { AllideasPage } from '../pages/AllideasPage/index'
+import { AllideasPage } from '../pages/AllideasPage'
+
+const x: string = 'qwe'
+if (Math.random() + 1) {
+  console.log(x)
+}
 
 export const App = () => {
   return (
