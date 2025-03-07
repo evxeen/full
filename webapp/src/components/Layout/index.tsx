f6dbd0ebe7509a5c6b3236155c602b6095071862
evxeen <evxeen@gmail.com>
2025-03-07 14:28:29 +0300

    Create layout & header

--- webapp/src/components/Layout/index.tsx
+++ webapp/src/components/Layout/index.tsx
@@ -1,23 +1,19 @@
-import { Link, Outlet } from 'react-router-dom'
 import css from './index.module.scss'
-import { getAllProductsRoute } from '../../../lib/routes.ts'
+import searchIcon from '../../../../../../icons/search.svg'
 
 export const Layout = () => {
   return (
     <div className={css.layout}>
       <div className={css.navigation}>
-        <div className={css.logo}>РМЗ</div>
-        <ul className={css.menu}>
-          <li className={css.item}>
-            <Link className={css.link} to={getAllProductsRoute()}>
-              Вся продукция
-            </Link>
-          </li>
-        </ul>
+        <div className={css.logo}>LOGO</div>
+        <div className={css.searchWrapper}>
+          <img src={searchIcon} alt="Поиск" className={css.searchIcon} />
+          <input type="text" placeholder="Поиск..." className={css.searchInput} />
+        </div>
       </div>
-      <div className={css.content}>
-        <Outlet />
-      </div>
+      {/* <div className={css.content}> */}
+      {/*  <Outlet /> */}
+      {/* </div> */}
     </div>
   )
 }
